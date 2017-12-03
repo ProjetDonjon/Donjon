6fb05fcff604d4ab1320fedc382c6a99cfd53182
Paul <paulbellot0@gmail.com>
2017-12-03 22:55:58 +0100

    First Commit

---
+++ Donjon_Majeure_Jeux_Videos/Assets/Ascenseur.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ascenseur : MonoBehaviour {
+	public Transform t;
+
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Transform> ();
+	}
+
+	void OnCollisionEnter(){
+		if (Input.GetKey ("e")) {
+			t.transform.position += new Vector3 (0, Mathf.Lerp (0f, 5f, 0.1f), 0);
+		}
+
+		if (Input.GetKey ("r")) {
+			t.transform.position -= new Vector3 (0, Mathf.Lerp (0f, 5f, 0.1f), 0);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		OnCollisionEnter();
+	}
+}
---
+++ Donjon_Majeure_Jeux_Videos/Assets/On_Button_Click.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class On_Button_Click : MonoBehaviour {
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(){
+		if (Input.GetKey ("1")) {
+			Destroy (transform.parent.gameObject);
+		}
+	}
+		
+	void Update () {
+		OnCollisionEnter ();
+	}
+}
